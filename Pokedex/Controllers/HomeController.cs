6335e992e1885ca5cf9a1a24068c3b90014912cb
MarianaDMG05 <mariana.gomes87@etec.sp.gov.br>
2024-03-04 20:19:42 -0300

    página 133

--- Pokedex/Controllers/HomeController.cs
+++ Pokedex/Controllers/HomeController.cs
@@ -37,8 +37,25 @@ public class HomeController : Controller
 
     public IActionResult Details(int id)
     {
+        List<Pokemon> pokemons = [];
+        using (StreamReader leitor = new("Data\\pokemons.json"))
+        {
+            string dados = leitor.ReadToEnd();
+            pokemons = JsonSerializer.Deserialize<List<Pokemon>>(dados);
+        }
+        List<Tipo> tipos = [];
+        using (StreamReader leitor = new("Data\\tipos.json"))
+        {
+            string dados = leitor.ReadToEnd();
+            tipos = JsonSerializer.Deserialize<List<Tipo>>(dados);
+        }
+        ViewData["Tipos"] = tipos;
+        var pokemon = pokemons
+        .Where(p => p.Numero == id)
+        .FirstOrDefault();
+        return View(pokemon);
+
         
-        return View();
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
